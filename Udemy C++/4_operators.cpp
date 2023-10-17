faef9c5d4bc01e04dd69aeaa446d6f1c8807f508
Vivek Singh <vvsvivek19@gmail.com>
2023-10-18 02:13:24 +0530

    more practice

---
+++ Udemy C++/4_operators.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=13,b=5;
+    float c;
+    c = a % b;
+    cout<<c<<endl;
+    return 0;
+}
---
+++ Udemy C++/5_area_of_circle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float Area,r;
+    cout<<"Please Enter the radius of the circle"<<endl;
+    cin>>r;
+    Area = 3.1425*r*r;
+    cout<<"Area of the circle is "<<Area<<endl;
+    return 0;
+
+}
