e571c6d0862fff60c5dec13a7499d7eacc0a9177
Vivek Singh <vvsvivek19@gmail.com>
2023-10-17 18:16:09 +0530

    More changes

---
+++ Udemy C++/1_myFirst.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+	int a,b,sum;
+	cout<<"Enter two numbers\n";
+	cin>>a>>b;
+	sum = a + b;
+	cout<<"Sum is "<<sum;
+	return 0;
+}
---
+++ Udemy C++/2_size_of_dataTypes.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    unsigned char x;
+    cout<<sizeof(x)<<endl;
+    cout<<CHAR_MIN<<" "<<UCHAR_MAX<<endl;
+    return 0;
+}
